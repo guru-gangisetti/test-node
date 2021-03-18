3254b62a3a7736f36443d15b33fe490f322dbfc3
guru-gangisetti <56891550+guru-gangisetti@users.noreply.github.com>
2021-03-18 14:08:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app =  express();
 
-const PORT = 5050; 
+const PORT = process.env.port || 5050; 
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
